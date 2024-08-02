84a0e12e4d75f91cb2a67c6ea35f15ff40f71449
Rei Ramadani <ramadanirei@gmail.com>
2024-08-02 17:47:38 +0200

    Changing the picture and the visual of the hero

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,29 +1,24 @@
 import React from "react";
-import Profile from "../assets/Profile.jpg";
+import Profile from "../assets/profile.png";
 
 const Hero = () => {
   return (
-    <div className="flex flex-row p-10 gap-10 bg-gray-400">
-      <img
-        src={Profile}
-        alt="Person"
-        className="h-auto max-w-md rounded-3xl "
-      />
+    <div className="flex flex-row p-10 gap-10 bg-gray-400 w-full h-full">
+      <img src={Profile} alt="Person" className="h-auto max-w-2xl" />
       <div className="flex flex-col justify-center gap-3 ">
-        <h1 className="text-lg font-semibold "> Rei Ramadani</h1>
+        <h1 className="text-2xl font-semibold "> Rei Ramadani</h1>
         <div className="text-5xl font-extrabold">
           <span className="text-blue-700">Frontend</span> Developer
         </div>
-        <p className="text-sm font-medium">
-          Hi, I'm Rei Ramadani, 20years old from Albania and I'm a Frontend
-          Developer.
+        <p className="text-md">
+          Hi, I'm Rei Ramadani, 20years old based on Tirana, Albania and I'm a
+          Frontend Developer.
         </p>
-        <button
-          href="#"
-          className="bg-blue-700 w-50 h-50 font-black rounded-3xl"
-        >
-          CONTACT ME
-        </button>
+        <div className="flex justify-start font-black">
+          <button className="p-3 bg-blue-700 rounded-sm" href="#">
+            CONTACT ME
+          </button>
+        </div>
       </div>
     </div>
   );
