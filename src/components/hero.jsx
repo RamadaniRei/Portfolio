1ae317aae8fa266525979f0c2c1b98711d7f8330
Rei Ramadani <ramadanirei@gmail.com>
2024-08-02 16:42:31 +0200

    Creating the hero feature of the page

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -3,15 +3,27 @@ import Profile from "../assets/Profile.jpg";
 
 const Hero = () => {
   return (
-    <div className="flex-row">
-      <img src={Profile} alt="Person" className="h-auto max-w-md " />
-      <div className="flex-col ">
-        <h2> Rei Ramadani</h2>
-        <div> Frontend Developer</div>
-        <p>
-          I'm Rei Ramadani, 20years old from Albania.I'm a Frontend Developer.{" "}
+    <div className="flex flex-row p-10 gap-10 bg-gray-400">
+      <img
+        src={Profile}
+        alt="Person"
+        className="h-auto max-w-md rounded-3xl "
+      />
+      <div className="flex flex-col justify-center gap-3 ">
+        <h1 className="text-lg font-semibold "> Rei Ramadani</h1>
+        <div className="text-5xl font-extrabold">
+          <span className="text-blue-700">Frontend</span> Developer
+        </div>
+        <p className="text-sm font-medium">
+          Hi, I'm Rei Ramadani, 20years old from Albania and I'm a Frontend
+          Developer.
         </p>
-        <button href="#" className="bg-red-50 w-50 h-50"></button>
+        <button
+          href="#"
+          className="bg-blue-700 w-50 h-50 font-black rounded-3xl"
+        >
+          CONTACT ME
+        </button>
       </div>
     </div>
   );
