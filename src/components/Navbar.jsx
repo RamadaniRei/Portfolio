16db0172dcbb3a672f569a9ac6bd27bd2ec4cd1a
Rei Ramadani <ramadanirei@gmail.com>
2024-08-02 15:08:45 +0200

    Visual changes to the navbar including padding and flex

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,25 +2,25 @@ import React from "react";
 import logo from "../assets/react.svg";
 const Navbar = () => {
   return (
-    <nav className="bg-blue-700 border-b border-indigo-500 pr-20">
-      <div className="flex justify-between">
-        <div>
-          <img src={logo} alt="logo" />
-        </div>
-        <div className="flex gap-20">
-          <div className="text-white decoration-white">
-            <a href="#">Home</a>
-          </div>
-          <div>
-            <a href="#">Projects</a>
-          </div>
-          <div>
-            <a href="#">Skills</a>
-          </div>
+    <nav className="flex justify-between w-full p-3 bg-blue-700">
+      <div>
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="flex gap-20">
+        <div className="text-white decoration-white">
+          <a href="#">Home</a>
         </div>
         <div>
-          <a href="#">Contact me</a>
+          <a href="#">Projects</a>
         </div>
+        <div>
+          <a href="#">Skills</a>
+        </div>
+      </div>
+      <div>
+        <a href="#" className="pr-5">
+          Contact me
+        </a>
       </div>
     </nav>
   );
