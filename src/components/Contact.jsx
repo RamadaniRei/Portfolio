daa3884def4e0904071c61cd1e7a82a8f5b17e94
Rei Ramadani <ramadanirei@gmail.com>
2024-08-05 13:02:03 +0200

    Fixing Message textarea

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div className="bg-gray-400 p-10 ">
+    <div className="flex justify-center bg-gray-400 p-10 ">
       <form className="flex flex-col  items-center gap-5">
         <h1 className="text-4xl font-extrabold">Contact us</h1>
         <div className="flex flex-row gap-20">
@@ -10,6 +10,7 @@ const Contact = () => {
             <label className="text-lg font-bold"> First Name </label>
             <input
               type="text"
+              placeholder="First Name"
               className="bg-white border-blue-700 text-black p-2 rounded-md"
             />
           </div>
@@ -17,6 +18,7 @@ const Contact = () => {
             <label className="text-lg font-bold"> Last Name </label>
             <input
               type="text"
+              placeholder="Last Name"
               className="bg-white border-blue-700 text-black p-2 rounded-md"
             />
           </div>
@@ -45,8 +47,9 @@ const Contact = () => {
             Message you want to send{" "}
           </label>
           <textarea
-            cols="50"
+            cols="62"
             rows="10"
+            placeholder="Leave us a message..."
             type="textarea"
             className="bg-white text-black rounded-md  "
           />
